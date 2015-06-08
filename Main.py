c83e0eff0d7a2b79e75f10b5fc94dd5817db653b
Jonathan Harris <Je.harris25@gmail.com>
2015-06-08 11:09:34 -0500

    Updated README, added Race Class

--- Main.py
+++ Main.py
@@ -1,14 +1,20 @@
-#Main RPG game
-#Jonathan Harris
-#June 7, 2015
+# Main RPG game
+# Jonathan Harris
+# June 7, 2015
 
 from __future__ import print_function
 
+class Race:
+    def __init__(self):
+        self.races = ["Human","Elf","Dwarf","Halfing", "Gnome"]
+
 class Player:
+
     def __init__(self):
         self.name = None
         self.playerClass = None
         self.health = 0
+        self.race = Race()
 
     def setName(self, playerName):
         self.name = playerName
@@ -17,11 +23,16 @@ class Player:
         return self.name
 
 
+
+
 if __name__ == '__main__':
     player = Player()
     print("Hello young sir/ma'am. Let me tell you a story.")
-    print("But first I must know your name. So tell me there. What is your name.")
+    print(
+        "But first I must know your name. So tell me there. What is your name.")
     name = raw_input()
     player.setName(name)
-    print("OK " + player.getName() + " but before we continue, you need a profession.")
-    print("Since this is a DnD type game, you need to choose a class and a race.")
+    print("OK " + player.getName() +
+          " but before we continue, you need a profession.")
+    print(
+        "Since this is a DnD type game, you need to choose a class and a race.")
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RPGGame-v2
+This is a revision of a game I made for a final project in the class *Intro to Computer Science* At Carleton College.
+The basic goal of the game is to progress through the story
+and reach the final boss. Unfortunately as of right now the story is linear in nature so the wrong choice can lead in death.
+Hope you will enjoy the remake. More features will be included as I see fit.
+
+
+#Mechanics
+- **Character Creation**
+- **Dice Mechanics**
+- **Weapon finding**
+- **Item Usage**
